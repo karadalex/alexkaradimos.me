d7391372dce148dc5f9c01201bd0a761650d62b7
Alex Karadimos <karadalex@gmail.com>
2016-07-31 10:37:55 +0300

    Bootstrapping Routes

--- src/server/main.js
+++ src/server/main.js
@@ -5,7 +5,14 @@ var helmet = require('helmet');
 // Routes
 var home = require('./routes/home');
 var about = require('./routes/about');
+var archive = require('./routes/archive');
+var blog = require('./routes/blog');
+var contact = require('./routes/contact');
+var cv = require('./routes/cv');
+var portfolio = require('./routes/portfolio');
+var press = require('./routes/press');
 var api = require('./routes/api');
+var errors = require('./routes/errors');
 
 var app = express();
 app.use(helmet());
@@ -13,6 +20,12 @@ app.use(helmet());
 // Routes
 app.use('/', home);
 app.use('/about', about);
+app.use('/archive', archive);
+app.use('/blog', blog);
+app.use('/contact', contact);
+app.use('/cv', cv);
+app.use('/portfolio', portfolio);
+app.use('/press',press);
 app.use('/api', api);
 
 // Static Files
@@ -21,23 +34,10 @@ app.use('/media', express.static(__dirname + '/../client/media'));
 app.use('/scripts', express.static(__dirname + '/../client/scripts'));
 app.use('/styles', express.static(__dirname + '/../client/styles'));
 
+// Errors Handling
+app.use('*', errors);
 
-/*******************************
-* Erros Section
-*******************************/
-app.get('*', function(req, res, next) {
-  var err = new Error();
-  err.status = 404;
-  next(err);
-});
-// handling 404 errors
-app.use(function(err, req, res, next) {
-  if(err.status !== 404) {
-    return next();
-  }
-  res.send(err.message || 'Page not Found');
-});
-
+// App Listening
 app.listen(3000, function () {
   console.log('Personal Web App listening on port 3000');
 });
