511d0706ac419ca713f830bee3e35a2c0a324c13
Alex Karadimos <karadalex@gmail.com>
2016-07-31 11:58:08 +0300

    Added home page

--- src/server/routes/home.js
+++ src/server/routes/home.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Initial Page
 router.get('/', function(req, res, next) {
   var options = {
     root: __dirname + '/../../client/',
@@ -18,6 +18,28 @@ router.get('/', function(req, res, next) {
     }
     else {
       console.log('GET', '/');
+    }
+  });
+});
+
+// Home Page
+router.get('/home', function(req, res, next) {
+  var options = {
+    root: __dirname + '/../../client/views',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+  var fileName = 'home.html';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('GET', '/'+fileName);
     }
   });
 });
